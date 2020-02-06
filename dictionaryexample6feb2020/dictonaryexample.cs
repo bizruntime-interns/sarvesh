edf8cfc85e5de2848b9cb2c1811886b04c9d0649
sarvesh <47808172+sarveshjha07@users.noreply.github.com>
2020-02-06 18:45:04 +0530

    Add files via upload

---
+++ dictionaryexample6feb2020/dictonaryexample.cs
@@ -0,0 +1,41 @@
+﻿using System;
+using System.Collections;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace dictionaryexample6feb
+{
+    public class customer
+    {
+        public string custid { get; set; }
+        public string custname { get; set; }
+        public int salary { get; set; }
+        public string dob { get; set; }
+    }
+    class Program
+    {
+        private static readonly log4net.ILog log =
+            log4net.LogManager.GetLogger(System.Reflection.MethodBase.GetCurrentMethod().DeclaringType);
+        static void Main(string[] args)
+        {
+            List<customer> customers = new List<customer>();
+            customer c1 = new customer { custid = "2016cse347", custname = "dinesh", salary = 4000, dob = "29/12/1999" };
+            customer c2 = new customer { custid = "2016cse452", custname = "baiju", salary = 40000, dob = "28/11/1999" };
+            customer c3 = new customer { custid = "2016cse324", custname = "suresh", salary = 47000, dob = "27/10/1998" };
+            customer c4 = new customer { custid = "2016cse4522", custname = "manas", salary = 45000, dob = "26/18/1994" };
+            customers.Add(c1);
+            customers.Add(c2);
+            customers.Add(c3);
+            customers.Add(c4);
+            foreach(customer obj in customers)
+            {
+                log.Info(obj.custid+" "+obj.custname+" "+obj.salary+" "+obj.dob);
+            }
+            Console.ReadLine();
+
+
+        }
+    }
+}
---
+++ dictionaryexample6feb2020/sorting.cs
@@ -0,0 +1,73 @@
+﻿using System;
+using System.Collections;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace sorting
+{
+    public class student:IComparable<student>
+    {
+        public int studtid { get; set; }
+        public string studname { get; set; }
+        public int marks { get; set; }
+        public string dob { get; set; }
+
+        public int CompareTo(student other)
+        {
+            if (this.studtid > other.studtid)
+            {
+                return 1;
+            }else if (this.studtid < other.studtid)
+            {
+                return -1;
+            }
+            else
+            {
+                return 0;
+            }
+        }
+    }
+    class comparestudents : IComparer<student>
+    {
+        public int Compare(student x, student y)
+        {
+            if (x.marks > y.marks)
+            {
+                return 1;
+            }
+            else if (x.marks < y.marks)
+            {
+                return -1;
+            }else
+            {
+                return 0;
+            }
+        }
+    }
+    class Program
+    {
+        private static readonly log4net.ILog log =
+            log4net.LogManager.GetLogger(System.Reflection.MethodBase.GetCurrentMethod().DeclaringType);
+        static void Main(string[] args)
+        {
+            
+            student s1 = new student { studtid = 34, studname = "dinesh", marks = 4000, dob = "29/12/1999" };
+            student s2 = new student { studtid = 47, studname = "baiju", marks = 40000, dob = "28/11/1999" };
+            student s3 = new student { studtid = 23, studname = "suresh", marks = 47000, dob = "27/10/1998" };
+            student s4 = new student { studtid = 32, studname = "manas", marks = 45000, dob = "26/18/1994" };
+            List<student> students = new List<student>() { s1, s2, s3, s4 };
+            comparestudents onj = new comparestudents();
+            students.Sort(onj);
+
+            foreach (student obj in students)
+            {
+                log.Info(obj.studtid + " " + obj.studname + " " + obj.marks  + " " + obj.dob);
+            }
+            Console.ReadLine();
+
+
+        }
+    }
+}
